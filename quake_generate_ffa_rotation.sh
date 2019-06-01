95925438060e8d40c66008f907e249559b9d4ee0
ciscon <dustin.glidden@gmail.com>
2019-06-01 15:15:37 -0400

    make fallback (first) map in rotation set even when shuffled always make variable even if unset/0 so we can exec this config while server is running and overwrite previous ones

--- quake_generate_ffa_rotation.sh
+++ quake_generate_ffa_rotation.sh
@@ -1,27 +1,66 @@
 #!/bin/bash
-# generate ffa map rotation using specified list in the following format:
+#generate ffa map rotation using specified list in the following format:
 #	mapname minplayers maxplayers
 
 #shuffle map cycle?
 shuffle=1
 
-#map list
-maps="ultrav
-spinev2 3 14
-shifter 3
-aerowalk 0 14
-dm2 3"
+#this is our fallback map
+firstmap="ultrav"
 
+maps="
+spinev2 3
+shifter 3
+ztndm5 2
+burialb2 0 14
+rwild 3
+qwdm1 3
+ztndm4 2
+aerowalk 0 14
+dm2 3
+debello 3
+ztndm2 4
+p3a 2
+a2 2
+pkeg1
+river 3
+ztndm3
+efdm8 3
+rf2 2
+subterfuge 2
+spitfire 3
+zen 3 10
+ztndm6 3
+trw 3
+ferrum 3
+bravado 0 14
+efdm10 3
+skullc 0 14
+factoryx 4
+q1q3thunderstruck 4
+utressor 3
+agenda 2
+lacrima 3
+zed2 3
+bless 3 14
+croctear 14
+warzone 2
+"
 
 shuffle_com="cat"
 if [ "$shuffle" -eq 1 ];then
 	shuffle_com="shuf"
 fi
-newmaps=$(echo "$maps"|sort -u|$shuffle_com)
+shuffled=$(echo "$maps"|sort -u|$shuffle_com)
+
+newmaps=$(echo -e "${firstmap}\n${shuffled}")
 
 count=0
 
 while read -r line; do
+	if [ -z "$line" ];then
+		continue
+	fi
 	map=""
 	min=""
 	max=""
@@ -40,11 +79,13 @@ while read -r line; do
 		fi
 	done
 	echo 'set k_ml_'$count' "'$map'"'
-	if [ ! -z $min ] && [ $min -ne 0 ];then
-		echo 'set k_ml_minp_'$count' "'$min'"'
+	if [ -z $min ];then
+		min=0
 	fi
-	if [ ! -z $max ] && [ $max -ne 0 ];then
-		echo 'set k_ml_maxp_'$count' "'$max'"'
+	echo 'set k_ml_minp_'$count' "'$min'"'
+	if [ -z $max ];then
+		max=0
 	fi
+	echo 'set k_ml_maxp_'$count' "'$max'"'
 	let count=count+1
 done <<< "$newmaps"
