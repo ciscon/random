48b9061349468ef0aadab75c238c9eb832f966fc
dustin glidden <dglidden@sitscape.com>
2019-06-11 17:34:25 -0400

    updated script

--- quake-get_qw_serverinfo.sh
+++ quake-get_qw_serverinfo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 quakestat -qwm master.quakeservers.net > /tmp/quakeservers
+quakestat -qwm qwmaster.fodquake.net >> /tmp/quakeservers
 
 server=""
 port=""
@@ -15,16 +16,16 @@ for both in $(awk -F'[ :]' '{print $3 , $4}' /tmp/quakeservers);do
 	fi
 	if [ ! -z "$server" ] && [ ! -z "$port" ];then
 		(
-		output=$(echo -e "\xff\xff\xff\xffstatus 23" | nc -w 2 -u $server $port | strings) 
-		admin=$(echo "$output"|awk -F'admin' '{print $2}'|awk -F'\' '{print $2}')
-		host=$(echo "$output"|awk -F'hostname' '{print $2}'|awk -F'\' '{print $2}')
+		output=$(echo -e "\xff\xff\xff\xffstatus 1" | nc -w 5 -u $server $port | strings) 
 		version=$(echo "$output"|awk -F'version' '{print $2}'|awk -F'\' '{print $2}')
-		if [ ! -z "$admin" ];then
-			echo "$version - $host - $server $port - $admin"
+		if [ ! -z "$version" ];then
+			echo "$version - $server $port"|grep -e '^QTV'
 		fi
 		)&
 		server=""
 		port=""
 		fi
 
-	done
+done
+
+wait
