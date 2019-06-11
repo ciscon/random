715158455b13e88d923b1eff580adbe729e55fee
dustin glidden <dglidden@sitscape.com>
2019-06-11 16:31:36 -0400

    script to pull qtv servers

---
+++ quake-get_qtv_servers.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+timeout=5
+
+echo > /tmp/quakeservers.1
+echo > /tmp/quakeservers
+quakestat -timeout $timeout -maxsim 100 -qwm qwmaster.fodquake.net|grep -v 'no response' >> /tmp/quakeservers 2>/dev/null&
+quakestat -timeout $timeout -maxsim 100 -qwm master.quakeservers.net|grep -v 'no response' >> /tmp/quakeservers 2>/dev/null&
+
+wait
+
+server=""
+port=""
+for both in $(awk -F'[ :]' '{print $3 , $4}' /tmp/quakeservers|sort -u);do 
+
+	if [ -z "$server" ];then
+		server=$both
+		continue
+	fi
+	if [ -z "$port" ];then
+		port=$both
+	fi
+	if [ ! -z "$server" ] && [ ! -z "$port" ];then
+		(
+		output=$(echo -e "\xff\xff\xff\xffstatus 32" | nc -w $timeout -u $server $port 2>/dev/null| strings) 
+		if [ ! -z "$output" ];then
+            if echo "$output"|grep -e '^nqtv' >/dev/null 2>&1;then
+			    echo "$server" >> /tmp/quakeservers.1
+            fi
+		fi
+		)&
+		server=""
+		port=""
+		fi
+
+done
+
+wait
+
+for server in $(sort -u /tmp/quakeservers.1);do
+    if [ "$(echo -e 'GET /\n'|nc -w $timeout $server 28000 2>/dev/null|grep -c 'nowplaying')" == "1" ];then
+        echo $server
+    fi
+done
