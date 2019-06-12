86775f5ab239230bbdb1c6e18f7320d2f9423226
dustin glidden <dglidden@sitscape.com>
2019-06-12 15:59:01 -0400

    update serverinfo script

--- quake-get_qw_serverinfo.sh
+++ quake-get_qw_serverinfo.sh
@@ -1,31 +1,66 @@
 #!/bin/bash
+# poll individual quake servers and get admin information, output in csv format
 
 quakestat -qwm master.quakeservers.net > /tmp/quakeservers
 quakestat -qwm master.quakeservers.net >> /tmp/quakeservers
 
 server=""
 port=""
-for both in $(awk -F'[ :]' '{print $3 , $4}' /tmp/quakeservers);do 
 
-    if [ -z $server ];then
-        server=$both
-        continue
+declare -A admins
+
+output=$(for both in $(awk -F'[ :]' '{print $3 , $4}' /tmp/quakeservers);do 
+        if [ -z $server ];then
+            server=$both
+            continue
+        fi
+        if [ -z $port ];then
+            port=$both
+        fi
+        if [ ! -z "$server" ] && [ ! -z "$port" ];then
+            (
+                output=$(echo -e "\xff\xff\xff\xffstatus 23" | nc -w 5 -u $server $port 2>/dev/null| strings) 
+                admin=$(echo "$output"|awk -F'admin' '{print $2}'|awk -F'\' '{print $2}')
+                host=$(echo "$output"|awk -F'hostname' '{print $2}'|awk -F'\' '{print $2}')
+                version=$(echo "$output"|awk -F'version' '{print $2}'|awk -F'\' '{print $2}')
+                if [ ! -z "$admin" ];then
+                    echo -e "$admin\t$version\t$host\t$server"|tr -d "\n"
+                    echo
+                fi
+            )&
+            server=""
+            port=""
+        fi
+        sleep .01
+    done)
+
+wait
+
+while read -r line; do
+
+    adminfull=$(echo -n "$line" | cut -f1 -d$'\t')
+
+    adminemail=$(echo "$adminfull"|awk -F '[<>]' '{print $2}')
+    admin=$(echo "$adminfull"|awk -F '[<>]' '{print $1}'|awk '{$1=$1};1')
+
+    if [ -z "$admin" ];then
+        admin="$adminemail"
     fi
-    if [ -z $port ];then
-        port=$both
-    fi
-    if [ ! -z "$server" ] && [ ! -z "$port" ];then
-        (
-            output=$(echo -e "\xff\xff\xff\xffstatus 23" | nc -w 5 -u $server $port | strings) 
-            admin=$(echo "$output"|awk -F'admin' '{print $2}'|awk -F'\' '{print $2}')
-            host=$(echo "$output"|awk -F'hostname' '{print $2}'|awk -F'\' '{print $2}')
-            version=$(echo "$output"|awk -F'version' '{print $2}'|awk -F'\' '{print $2}')
-            if [ ! -z "$admin" ];then
-                echo "$version - $host - $server $port - $admin"
-            fi
-        )&
-        server=""
-        port=""
+    if [ ! -z "$adminemail" ];then
+        key="$adminemail"
+    else
+        key="$admin"
     fi
 
+    if [ ! -z "$key" ];then
+        version=$(echo -n "$line" | cut -f2 -d$'\t')
+        host=$(echo -n "$line" | cut -f3 -d$'\t')
+        server=$(echo -n "$line" | cut -f4 -d$'\t')
+        admins[$key]="$admin,$adminemail,$version,$server,$host"
+    fi
+
+done <<< "$output"
+
+for adminline in "${!admins[@]}";do
+    echo ${admins[$adminline]}
 done
