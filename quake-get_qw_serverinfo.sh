15a9d504d52f932ecb43d44c841b2177044a9a54
dustin glidden <dglidden@sitscape.com>
2019-06-12 15:13:56 -0400

    updated qw serverinfo script

--- quake-get_qw_serverinfo.sh
+++ quake-get_qw_serverinfo.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
 quakestat -qwm master.quakeservers.net > /tmp/quakeservers
-quakestat -qwm qwmaster.fodquake.net >> /tmp/quakeservers
+quakestat -qwm master.quakeservers.net >> /tmp/quakeservers
 
 server=""
 port=""
 for both in $(awk -F'[ :]' '{print $3 , $4}' /tmp/quakeservers);do 
 
-	if [ -z $server ];then
-		server=$both
-		continue
-	fi
-	if [ -z $port ];then
-		port=$both
-	fi
-	if [ ! -z "$server" ] && [ ! -z "$port" ];then
-		(
-		output=$(echo -e "\xff\xff\xff\xffstatus 1" | nc -w 5 -u $server $port | strings) 
-		version=$(echo "$output"|awk -F'version' '{print $2}'|awk -F'\' '{print $2}')
-		if [ ! -z "$version" ];then
-			echo "$version - $server $port"|grep -e '^QTV'
-		fi
-		)&
-		server=""
-		port=""
-		fi
+    if [ -z $server ];then
+        server=$both
+        continue
+    fi
+    if [ -z $port ];then
+        port=$both
+    fi
+    if [ ! -z "$server" ] && [ ! -z "$port" ];then
+        (
+            output=$(echo -e "\xff\xff\xff\xffstatus 23" | nc -w 5 -u $server $port | strings) 
+            admin=$(echo "$output"|awk -F'admin' '{print $2}'|awk -F'\' '{print $2}')
+            host=$(echo "$output"|awk -F'hostname' '{print $2}'|awk -F'\' '{print $2}')
+            version=$(echo "$output"|awk -F'version' '{print $2}'|awk -F'\' '{print $2}')
+            if [ ! -z "$admin" ];then
+                echo "$version - $host - $server $port - $admin"
+            fi
+        )&
+        server=""
+        port=""
+    fi
 
 done
-
-wait
