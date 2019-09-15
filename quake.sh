2d7db16ae40bce50006133048ddea2236f11678f
ciscon <dustin.glidden@gmail.com>
2019-09-15 16:43:13 -0400

    syntax

--- quake.sh
+++ quake.sh
@@ -126,7 +126,7 @@ fi
 
 if [ $nvidia_settings_optimizations -eq 1 ];then
 	#nvidia: set max performance in case we already haven't, must be set per gpu
-	nvidia-settings -a [gpu:0]GPUPowerMizerMode=1 >/dev/null 2>&1
+	nvidia-settings -a [gpu:0]/GPUPowerMizerMode=1 >/dev/null 2>&1
 	#set performance over quality
 	nvidia-settings -a OpenGLImageSettings=3 >/dev/null 2>&1
 	#no buffer swaps
