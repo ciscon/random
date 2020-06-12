c0f175badd7ce4ad269c2730c4ab7172f8c32ab4
dustin glidden <dglidden@sitscape.com>
2020-06-12 13:18:09 -0400

    update libvirt vm mac/ip gathering script to support proxy host

--- libvirt-get_vm_mac_ip.sh
+++ libvirt-get_vm_mac_ip.sh
@@ -1,15 +1,17 @@
 #!/bin/bash 
 # ssh to libvirt hosts and retrieve mac/ip addresses for all vms
 
+#vm network prefix
 network_prefix="10.10.10"
 
+#vm hosts
 hosts="
-vm-host2
-vm-host3
 vm-host4
 vm-host5
 vm-host6
 "
+#host to ssh into to do arp resolution, must be on same network as vms
+proxy_host="dalek"
 
 
 for i in $hosts;do
@@ -19,7 +21,8 @@ for i in $hosts;do
         if [ -z "$vms" ];then
             errors+="failed to retrieve vm list from $i."
         fi
-        ssh "$i" "for ip in ${network_prefix}.{1..254}; do ping -W1 -c1 \${ip} >/dev/null 2>&1 & done;wait"
+        host=${proxy_host:-$i}
+        ssh "$host" "for ip in ${network_prefix}.{1..254}; do ping -W1 -c1 \${ip} >/dev/null 2>&1 & done;wait"
         for vm in $vms;do
             if [ ! -z "$vm" ];then
                 (
@@ -27,7 +30,7 @@ for i in $hosts;do
                     if [ -z "$mac" ];then
                         errors+="no mac found for $vm\n"
                     fi
-                    ip=$(ssh "$i" "arp -n|grep --color=never -i \"$mac\" 2>/dev/null|tail -n1|awk '{print \$1}' 2>/dev/null")
+                    ip=$(ssh "$host" "arp -n|grep --color=never -i \"$mac\" 2>/dev/null|tail -n1|awk '{print \$1}' 2>/dev/null")
                     if [ -z "$ip" ];then
                         errors+="no ip found for $vm\n"
                     else
