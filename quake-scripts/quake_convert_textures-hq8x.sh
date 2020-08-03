001f54a1cc9402b1551dbcfe453f5ff35b17531c
ciscon <ciscon@nicotinelounge.com>
2020-08-03 19:31:33 -0400

    comments

--- quake-scripts/quake_convert_textures-hq8x.sh
+++ quake-scripts/quake_convert_textures-hq8x.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-#requires pakextract, bsputil, and qpakman
+#requires pakextract, bsputil, optipng, and qpakman
 
 if [ ! -f pak0.pak ];then
 	echo "no pak file"
@@ -8,27 +8,39 @@ fi
 if [ -d maps ];then
 	rm -rf maps
 fi
+
 for i in *.pak;do pakextract $i;done 
 cd maps
+
+#other bsps?
+#cp /path/to/other/bsps/*.bsp .
+
 for i in *.bsp;do bsputil --extract-textures $i;done 
 for i in *.wad;do qpakman -f -e "$i";done 
 
-#greyscale
+#greyscale?
 #for i in *.png;do convert $i -set colorspace Gray -separate -average $i;done 
 
 for i in *.png;do 
 	#hq8x
 	convert $i -magnify -magnify -magnify $i
 done 
+
 if [ -d textures ];then
 	rm -rf textures
 fi
 mkdir textures 
 mv *.png textures 
 cd textures
+
+#optimize pngs
+optipng -strip all *.png
+
+#fix names
 rename 's/plus_/+/' *.png
 rename 's/star_/#/' *.png
 rename 's/_fbr//' *.png
+
 cd ..
 
 zip -r ../scale8x.pk3 textures 
