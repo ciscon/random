8c5dad8a4da79f909f9d369d60328b10b16c2ea2
ciscon <ciscon@nicotinelounge.com>
2020-08-03 19:06:33 -0400

    scale quake textures hq8x

---
+++ quake-scripts/quake_convert_textures-hq8x.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -e
+#requires pakextract, bsputil, and qpakman
+
+if [ ! -f pak0.pak ];then
+	echo "no pak file"
+fi
+
+if [ -d maps ];then
+	rm -rf maps
+fi
+for i in *.pak;do pakextract $i;done 
+cd maps
+for i in *.bsp;do bsputil --extract-textures $i;done 
+for i in *.wad;do qpakman -f -e "$i";done 
+
+#greyscale
+#for i in *.png;do convert $i -set colorspace Gray -separate -average $i;done 
+
+for i in *.png;do 
+	#hq8x
+	convert $i -magnify -magnify -magnify $i
+done 
+if [ -d textures ];then
+	rm -rf textures
+fi
+mkdir textures 
+mv *.png textures 
+cd textures
+rename 's/plus_/+/' *.png
+rename 's/_fbr//' *.png
+cd ..
+
+zip -r ../scale8x.pk3 textures 
