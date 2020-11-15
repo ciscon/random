6c062a59e10a8893a60fe037c632426a97e72d5c
ciscon <ciscon@nicotinelounge.com>
2020-11-15 14:25:54 -0500

    ezquake appimage creation script

---
+++ quake-scripts/ezquake-generate-appimage/manual_creation.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+if ! hash appimagetool;then
+	echo "appimagetool not installed."
+	exit 1
+fi
+
+DESKTOP_ENTRY="[Desktop Entry]
+Name=ezquake
+Exec=ezquake-linux-x86_64
+Icon=quake
+Type=Application
+Categories=Game;"
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+mkdir -p build && \
+mkdir -p AppDir/usr/bin && \
+mkdir -p AppDir/usr/lib && \
+cd build && \
+if [ ! -d ezquake-source ];then
+	git clone https://github.com/ezQuake/ezquake-source.git
+fi
+cd ezquake-source && \
+git clean -qfdx
+git reset --hard
+git pull
+if [ $? -ne 0 ];then
+	echo "error updating from git"
+	exit 1
+fi
+REVISION=$(git log -n 1|head -1|awk '{print $2}'|cut -c1-6)
+make clean && \
+make -j5 && \
+cp -f ezquake-linux-x86_64 "$DIR/AppDir/usr/bin/." && \
+ln -sf usr/bin/ezquake-linux-x86_64 "$DIR/AppDir/AppRun" && \
+echo "$DESKTOP_ENTRY" > "$DIR/AppDir/ezquake.desktop" && \
+cp "$DIR/quake.png" "$DIR/AppDir/."||true #copy over quake png if it exists
+cd "$DIR" && \
+ldd AppDir/usr/bin/ezquake-linux-x86_64 |grep --color=never -v libGL|grep --color=never -v libc.so|awk '{print $3}'|xargs -I% cp "%" AppDir/usr/lib/. && \
+appimagetool AppDir ezquake-$REVISION.AppImage
