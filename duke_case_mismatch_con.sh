20fbff5a0844c4bac26f339fe07384944c887b5d
ciscon <ciscon@nicotinelounge.com>
2020-12-26 16:55:27 -0500

    first do search/replace on basename

--- duke_case_mismatch_con.sh
+++ duke_case_mismatch_con.sh
@@ -3,26 +3,44 @@
 
 con_files="$(find . -type f -iname *.con -printf '%P\t')"
 files="$(find . -type f -printf '%P\t')"
+basename_files="$(find . -type f -printf '%f\t')"
 OLD_IFS=$IFS
 IFS=$'\t'
 
 for con in $con_files;do
   (
-  for file in $files;do
-    found=$(grep --color=never -i "$file" "$con")
+  #first check/fix only filenames
+  for file in $basename_files;do
+    found=$(grep --color=never -o -i "\s$file\s" "$con"|tr '\n' '\t')
     if [ ! -z "$found" ];then
       for foundfile in $found;do
-        grep --color=never "$file" "$con" >/dev/null 2>&1
+        grep --color=never "\s$file\s" "$con" >/dev/null 2>&1
         if [ $? -ne 0 ];then
           echo "case mismatch file/path on $file in $con"
           #optionally add rename or sed logic here
-          #sed -i "s|$foundfile|$file|g" "$con"
+          sed -i "s|$foundfile| $file |g" "$con"
+        fi
+      done
+    fi
+  done
+
+  #now entire paths
+  for file in $files;do
+    found=$(grep --color=never -o -i "\s$file\s" "$con"|tr '\n' '\t')
+    if [ ! -z "$found" ];then
+      for foundfile in $found;do
+        grep --color=never "\s$file\s" "$con" >/dev/null 2>&1
+        if [ $? -ne 0 ];then
+          echo "case mismatch file/path on $file in $con"
+          #optionally add rename or sed logic here
+          sed -i "s|$foundfile| $file |g" "$con"
         fi
       done
     fi
   done
   )&
   if (( $(wc -w <<<$(jobs -p)) % $(nproc) == 0 )); then wait; fi
+
 done
 
 wait
