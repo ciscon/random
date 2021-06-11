a734abfbd86dfa94d828947290644287c7907678
ciscon <ciscon@nicotinelounge.com>
2021-06-11 14:43:45 -0400

    loop

--- quake-scripts/quake-badplace_find_users_server.sh
+++ quake-scripts/quake-badplace_find_users_server.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 searchUser="player"
+waitTime=60
 
-wget -O - 'https://badplace.eu/api/v2/serverbrowser/busy'| \
-    jq '.[] as $parent | $parent.Players[].Name | select(. == "'$searchUser'") | $parent.Address'
+while [ 1 ];do
+  found=$(wget -q -O - 'https://badplace.eu/api/v2/serverbrowser/busy'| \
+      jq '.[] as $parent | $parent.Players[].Name | select(. == "'$searchUser'") | $parent.Address')
+  if [ "$lastfound" != "$found" ];then
+    notify-send "$searchUser on $found"
+  fi
+  lastfound=$found
+  sleep $waitTime
+done
