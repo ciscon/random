0716dfc21cc55699d10e5821a9a2c9cd589ed1a9
ciscon <ciscon@nicotinelounge.com>
2021-06-11 14:46:57 -0400

    no notify on empty

--- quake-scripts/quake-badplace_find_users_server.sh
+++ quake-scripts/quake-badplace_find_users_server.sh
@@ -6,7 +6,7 @@ waitTime=60
 while [ 1 ];do
   found=$(wget -q -O - 'https://badplace.eu/api/v2/serverbrowser/busy'| \
       jq '.[] as $parent | $parent.Players[].Name | select(. == "'$searchUser'") | $parent.Address')
-  if [ "$lastfound" != "$found" ];then
+  if [ "$lastfound" != "$found" ] && [ ! -z "$found" ];then
     notify-send "$searchUser on $found"
   fi
   lastfound=$found
