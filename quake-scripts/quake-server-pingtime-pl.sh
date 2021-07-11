f5c20826c12ec5ea4e2ab24aec4862c9e4b0e2af
ciscon <ciscon@nicotinelounge.com>
2021-07-11 17:01:24 -0400

    quake script to test response time or lack of response (pl)

---
+++ quake-scripts/quake-server-pingtime-pl.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#test response time of qw server on given ports, output if there is no response or above rtt threshold
+
+host=quakecon.miami.ultrav.us
+ports="28501 28502"
+rtt_threshold=10
+sleeptime=1
+
+while [ 1 ];do
+
+  for port in $ports;do
+
+    current=$(date +%s%N|cut -b1-13)
+    rtt=$(
+      echo -e "\xff\xff\xff\xffstatus 23"|nc -w 1 -u $host $port|stdbuf -i0 -o0 -e0 strings| \
+        while read -t 3 -r line;do
+
+        rtt=$(echo "scale=100;$(date +%s%N|cut -b1-13)-${current}"|bc)
+        echo "$rtt"
+        break
+
+      done
+    )
+
+    #no response?
+    if [ -z "$rtt" ];then rtt=999;fi
+
+    if [ $rtt -gt $rtt_threshold ];then
+      echo "$(date) : $host $port rtt=$rtt"
+    fi
+
+    sleep $sleeptime
+
+  done
+done
--- quake-scripts/quake-test_client_ports.sh
+++ quake-scripts/quake-test_client_ports.sh
@@ -2,10 +2,10 @@
 
 #uses: hping3, sudo, awk, sort
 
-host=london.badplace.eu
+host=quakecon.miami.ultrav.us
 port=28501
 start=27000
-stop=27100
+stop=27500
 
 #clientport scanner
 for clientport in $(seq $start $stop);do echo -n "client port=$clientport ";echo -e "\xff\xff\xff\xffstatus 23"|sudo hping3 -2 $host -p $port -E /dev/stdin -d 10 -c 1 -s $clientport 2>/dev/null|grep --color=never 'rtt=';done|awk -F'[= ]' '{print $16,":",$3}'|sort -nr
