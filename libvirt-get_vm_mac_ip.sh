3578a5207e180a500c39be92e4bd42bddd744604
ciscon <ciscon@nicotinelounge.com>
2021-09-28 16:30:29 -0400

    add operating system check

--- libvirt-get_vm_mac_ip.sh
+++ libvirt-get_vm_mac_ip.sh
@@ -13,6 +13,16 @@ vm-host6
 #host to ssh into to do arp resolution, must be on same network as vms
 proxy_host="dalek"
 
+#whether or not to attempt to ssh into the remote host to get version, if not set to 1 we will only classify the vm as linux/windows based on port 22 being open
+get_os_version=1
+
+if [ "$get_os_version" == 1 ];then
+	#start up ssh agent if we're checking os version
+	eval "$(ssh-agent -s)" >/dev/null 2>&1
+	ssh-add "$HOME/.ssh/id_rsa" >/dev/null 2>&1
+fi
+
+
 for i in $hosts;do
     if [ ! -z "$i" ];then
 
@@ -36,29 +46,46 @@ for i in $hosts;do
 		#ssh into host and get ips
 		echo "$vms"|ssh "$host" '
 
-		#populate arp table on host
-		for ip in '${network_prefix}'.{1..254}; do ping -W1 -c1 \${ip} >/dev/null 2>&1 & done;wait
+			#populate arp table on host
+			for ip in '${network_prefix}'.{1..254}; do ping -W1 -c1 \${ip} >/dev/null 2>&1 & done;wait
 
-		#get ips from arp table
-		while read line;do
-		(
-            if [ ! -z "$line" ];then
-				IFS=" " read name mac <<< $(echo "$line")
-                    ip=$(PATH=$PATH:/usr/sbin:/sbin arp -n|grep --color=never -i "$mac" 2>/dev/null|cut -d " " -f1|tail -n1 2>/dev/null)
-                    if [ -z "$ip" ];then
-						ip="NOTFOUND"
-						os="NOTFOUND"
-					else
-						#check ip for port 22
-						if hash nc;then 2>/dev/null
-							os=$(echo "garbage"|nc -w 1 $ip 22 >/dev/null 2>&1&&echo lin||echo win)
+			#get ips from arp table
+			while read line;do
+			(
+        	    if [ ! -z "$line" ];then
+					IFS=" " read name mac <<< $(echo "$line")
+        	            ip=$(PATH=$PATH:/usr/sbin:/sbin arp -n|grep --color=never -i "$mac" 2>/dev/null|cut -d " " -f1|tail -n1 2>/dev/null)
+        	            if [ -z "$ip" ];then #no ip found
+							ip="NOTFOUND"
+							os="NOTFOUND"
+						else
+							if [ "'$get_os_version'" == "1" ];then #get linux version
+								os=$(ssh -q -o ConnectTimeout=2 $ip '\''
+								if [ -f /etc/debian_version ];then
+									if [ -f /etc/os-release ];then
+										. /etc/os-release
+									else
+										ID=debian
+										VERSION_ID=$(cat /etc/debian_version)
+									fi
+									echo -n "$ID-$VERSION_ID"
+								elif [ -f /etc/redhat-release ];then
+									ver=$(cat /etc/redhat-release)
+									echo -n "$(echo $ver|tr "[:upper:]" "[:lower:]"|cut -d " " -f1)-$(echo $ver|sed "s/[^0-9.]//g")"
+								fi'\''||echo "windows")
+							else ##fall back to checking ip for port 22
+							if hash nc;then 2>/dev/null
+								os=$(echo "garbage"|nc -w 1 $ip 22 >/dev/null 2>&1&&echo linux||echo windows)
+							else
+								os="NOTFOUND"
+							fi
 						fi
-					fi
-                    echo -e "'$i'\t$name\t$ip\t$mac\t$os"
-            fi
-		)&
-        done'
-    fi
+				fi
+				echo -e "'$i'\t$name\t$ip\t$mac\t$os"
+				fi
+			)&
+		done'
+	fi
 done
 
 if [ ! -z "$errors" ];then
