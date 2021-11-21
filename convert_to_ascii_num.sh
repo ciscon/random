f0b014b73d945006d9e89a5897250056fb2aaefa
ciscon <ciscon@nicotinelounge.com>
2021-11-21 14:59:32 -0500

    move git clone out of if

--- convert_to_ascii_num.sh
+++ convert_to_ascii_num.sh
@@ -2,6 +2,6 @@
 #convert text/command to ascii numbers - example of why scrubbing scripts isn't quite good enough
 #to run: eval $'outputofcommand'
 
-string="while [ 1 ];do echo \"virus!\";done"
+string="while [ 1 ];do echo \"just doing some bitcoin mining...\";done"
 
 echo "$string"|sed 's/\(.\)/\1\n/g'|while read i;do if [ -z "$i" ];then i=" ";fi;printf "\\%o" "'${i}";done;echo ""
