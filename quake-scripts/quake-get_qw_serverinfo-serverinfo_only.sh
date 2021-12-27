babc342836da027ae2338a01f2cc342360e7ab5a
ciscon <ciscon@nicotinelounge.com>
2021-12-27 15:59:30 -0500

    make sure output is atomic

--- quake-scripts/quake-get_qw_serverinfo-serverinfo_only.sh
+++ quake-scripts/quake-get_qw_serverinfo-serverinfo_only.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 # poll individual quake servers and get admin information, output in csv format
 
-delimiter=$'\t'
+delimiter="\t"
+fileoutput=/dev/stdout
+
+#if output is a file, truncate it
+if [ -f "$output" ];then
+	echo -n > "$output"
+fi
 
 if hash geoiplookup 2>/dev/null;then
 	lookup=1
@@ -11,13 +17,12 @@ else
 	lookuptext=
 fi
 
-
 quakestat -qwm master.quakeservers.net > /tmp/quakeservers.tmp
 quakestat -qwm qwmaster.fodquake.net >> /tmp/quakeservers.tmp
 awk -F'[ :]' '{print $3 , $4}' /tmp/quakeservers.tmp | sort -u > /tmp/quakeservers.tmp.1
 
 #header
-echo "${lookuptext}host${delimiter}server${delimiter}port${delimiter}version"
+echo -e "${lookuptext}host${delimiter}server${delimiter}port${delimiter}version" >> "$fileoutput"
 
 for both in $(awk -F'[ ]' '{print $1 , $2}' /tmp/quakeservers.tmp.1);do
 	if [ -z $server ];then
@@ -33,17 +38,22 @@ for both in $(awk -F'[ ]' '{print $1 , $2}' /tmp/quakeservers.tmp.1);do
 			host=$(echo "$output"|awk -F'hostname' '{print $2}'|awk -F'\' '{print $2}')
 			version=$(echo "$output"|awk -F'version' '{print $2}'|awk -F'\' '{print $2}')
 			if [ ! -z "$host" ];then
+				locationoutput=
 				if [ $lookup -eq 1 ];then
 					location=$(geoiplookup $server|awk -F': ' '{print $2}')
-					echo -en "${location}${delimiter}"
+					if [ ! -z "$location" ];then
+						locationoutput="${location}"
+					else
+						locationoutput="N/A"
+					fi
 				fi
-				echo -e "${host}${delimiter}${server}${delimiter}${port}${delimiter}${version}"
+				echo -e "${locationoutput}${delimiter}${host}${delimiter}${server}${delimiter}${port}${delimiter}${version}" >> $fileoutput
 			fi
 		)&
 		server=""
 		port=""
 	fi
 	sleep .01
-done
+done | sort
 
 wait
