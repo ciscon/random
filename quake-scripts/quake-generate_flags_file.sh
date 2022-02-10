11fbefe9821a935f247ea91feecfea79ef91c4b7
ciscon <ciscon@nicotinelounge.com>
2022-02-10 02:53:56 -0500

    use svg as source

--- quake-scripts/quake-generate_flags_file.sh
+++ quake-scripts/quake-generate_flags_file.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 tempdir="/tmp/quakeflags"
+pngsize="115x60"
+
 rm -rf "$tempdir"
 
 mkdir -p "${tempdir}/textures/scoreboard/flags"
@@ -9,8 +11,11 @@ contents=''
 while read na url name
 do
 	lower=$(echo $name|tr '[:upper:]' '[:lower:]')
-	curl -s "$url" > "${tempdir}/textures/scoreboard/flags/${lower}.png"
-	convert "${tempdir}/textures/scoreboard/flags/${lower}.png" "${tempdir}/textures/scoreboard/flags/${lower}.png"
+	urlsvg=$(echo "${url%.png}"| sed 's/\.svg\/[0-9].*/\.svg/g;s/\/thumb//g'  )
+	curl -s "$urlsvg" > "${tempdir}/textures/scoreboard/flags/${lower}.svg"
+	convert -resize $pngsize "${tempdir}/textures/scoreboard/flags/${lower}.svg" -normalize "${tempdir}/textures/scoreboard/flags/${lower}.png"
+	pngquant --ext .png --force "${tempdir}/textures/scoreboard/flags/${lower}.png"
+	rm -f "${tempdir}/textures/scoreboard/flags/${lower}.svg"
 	contents+=$(echo -e '{ "code": "'${lower}'", "file": "flags/'${lower}'.png" },')
 done < <(curl -s https://en.wikipedia.org/wiki/ISO_3166-1|tr -d '\n'|sed 's/flagicon/\n|||/g'|grep '^|||'|sed 's/.*alt="" src="\([^"]*\)\".*ISO 3166-2:\([^"]*\)">.*/flag https:\1 \2 /g'|grep '^flag' --color=never)
 
