d56a8a00c4842e2b42090a4d55a1127af66f24ea
ciscon <ciscon@nicotinelounge.com>
2022-02-13 14:04:36 -0500

    get 3166-2 flags, support flags that don't have corresponding svg, just use whatever they have

--- quake-scripts/quake-generate_flags_file.sh
+++ quake-scripts/quake-generate_flags_file.sh
@@ -7,17 +7,40 @@ rm -rf "$tempdir"
 
 mkdir -p "${tempdir}/textures/scoreboard/flags"
 echo "[" > "${tempdir}/textures/scoreboard/flags.json"
+
 contents=''
+
+#iso 3166-1
 while read na url name
 do
+
+	echo "$name"
 	lower=$(echo $name|tr '[:upper:]' '[:lower:]')
 	urlsvg=$(echo "${url%.png}"| sed 's/\.svg\/[0-9].*/\.svg/g;s/\/thumb//g'  )
-	curl -s "$urlsvg" > "${tempdir}/textures/scoreboard/flags/${lower}.svg"
-	convert -resize $pngsize "${tempdir}/textures/scoreboard/flags/${lower}.svg" -normalize "${tempdir}/textures/scoreboard/flags/${lower}.png"
+	curl -s "$urlsvg" > "${tempdir}/textures/scoreboard/flags/${lower}.${urlsvg##*.}"
+	convert -resize $pngsize "${tempdir}/textures/scoreboard/flags/${lower}.${urlsvg##*.}" -normalize "${tempdir}/textures/scoreboard/flags/${lower}.png"
 	pngquant --ext .png --force "${tempdir}/textures/scoreboard/flags/${lower}.png"
-	rm -f "${tempdir}/textures/scoreboard/flags/${lower}.svg"
+	rm -f "${tempdir}/textures/scoreboard/flags/${lower}.${urlsvg##*.}"
 	contents+=$(echo -e '{ "code": "'${lower}'", "file": "flags/'${lower}'.png" },')
-done < <(curl -s https://en.wikipedia.org/wiki/ISO_3166-1|tr -d '\n'|sed 's/flagicon/\n|||/g'|grep '^|||'|sed 's/.*alt="" src="\([^"]*\)\".*ISO 3166-2:\([^"]*\)">.*/flag https:\1 \2 /g'|grep '^flag' --color=never)
+	country=$name
+
+	#iso 3166-2
+	while read na name url
+	do
+
+		echo "$name"
+	    lower=$(echo $name|tr '[:upper:]' '[:lower:]')
+		if [ -f "${tempdir}/textures/scoreboard/flags/${lower}.svg" ];then continue;fi
+	    urlsvg=$(echo "${url%.png}"| sed 's/\.svg\/[0-9]*px.*/\.svg/g;s/\/thumb//g')
+		echo "$urlsvg"
+	    curl -s "$urlsvg" > "${tempdir}/textures/scoreboard/flags/${lower}."${urlsvg##*.}""
+	    convert -resize $pngsize "${tempdir}/textures/scoreboard/flags/${lower}.${urlsvg##*.}" -normalize "${tempdir}/textures/scoreboard/flags/${lower}.png"
+	    pngquant --ext .png --force "${tempdir}/textures/scoreboard/flags/${lower}.png"
+	    rm -f "${tempdir}/textures/scoreboard/flags/${lower}.${urlsvg##*.}"
+	    contents+=$(echo -e '{ "code": "'${lower}'", "file": "flags/'${lower}'.png" },')
+	done < <(curl -s https://en.wikipedia.org/wiki/ISO_3166-2:$country|tr -d '\n'|sed 's/<span class="monospaced">'$country'-/\n|||'$country'-/g'|grep --color=never '^|||'|sed 's/|||\('$country'-[A-Z0-9]*\).*src="\/\/upload\([^"]*\)".*/flag \1 https:\/\/upload\2/g'|grep --color=never '^flag')
+
+done < <(curl -s https://en.wikipedia.org/wiki/ISO_3166-1|tr -d '\n'|sed 's/flagicon/\n|||/g'|grep --color=never '^|||'|sed 's/.*alt="" src="\([^"]*\)\".*ISO 3166-2:\([^"]*\)">.*/flag https:\1 \2 /g'|grep '^flag' --color=never)
 
 echo -e "${contents::-1}\n]" >> "${tempdir}/textures/scoreboard/flags.json"
 
