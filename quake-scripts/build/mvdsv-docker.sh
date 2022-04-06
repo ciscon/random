46905c091ec7caac342dd1eb2121b3bfe4eb08b0
ciscon <ciscon@nicotinelounge.com>
2022-04-06 18:44:05 -0400

    formatting

--- quake-scripts/build/mvdsv-docker.sh
+++ quake-scripts/build/mvdsv-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# build mvdsv for various architectures via docker                                                                                                                                                                                                                               
+# build mvdsv for various architectures via docker
 
 dist="debian"
 release="testing"
@@ -11,16 +11,16 @@ gitdir="$PWD/mvdsv"
 mkdir -p $outputdir $gitdir
 
 if [ ! -d $gitdir/.git ];then
-    echo "git directory for mvdsv not found, please clone to $gitdir"
-    exit 1
+	echo "git directory for mvdsv not found, please clone to $gitdir"
+	exit 1
 fi
 
 for arch in $archs;do
-    docker pull $arch/$dist:$release
-    docker run --net=host --rm --device /dev/fuse -v "$gitdir":/mvdsv $arch/$dist:$release bash -c 'export DEBIAN_FRONTEND=noninteractive;mkdir -p /etc/apt/apt.conf.d;echo "APT::Install-Recommends "0"; APT::AutoRemove::RecommendsImportant "false";" >> /etc/apt/apt.conf.d/01lean && apt-get -qqy update && apt-get -qqy install cmake build-essential libcurl4-openssl-dev && ln -sf "$(which make)" /usr/bin/gmake && cd /mvdsv && cmake -B build-'$arch' && cd build-'$arch' && make -j$(nproc) && chown -Rf '$(id -u ${USER})':'$(id -g ${USER})' /mvdsv/build*'
-    errorcode=$?
-    if [ $errorcode -ne 0 ];then
-        echo "$arch $errorcode"
-        exit 2
-    fi
+	docker pull $arch/$dist:$release
+	docker run --net=host --rm --device /dev/fuse -v "$gitdir":/mvdsv $arch/$dist:$release bash -c 'export DEBIAN_FRONTEND=noninteractive;mkdir -p /etc/apt/apt.conf.d;echo "APT::Install-Recommends "0"; APT::AutoRemove::RecommendsImportant "false";" >> /etc/apt/apt.conf.d/01lean && apt-get -qqy update && apt-get -qqy install cmake build-essential libcurl4-openssl-dev && ln -sf "$(which make)" /usr/bin/gmake && cd /mvdsv && cmake -B build-'$arch' && cd build-'$arch' && make -j$(nproc) && chown -Rf '$(id -u ${USER})':'$(id -g ${USER})' /mvdsv/build*'
+	errorcode=$?
+	if [ $errorcode -ne 0 ];then
+		echo "$arch $errorcode"
+		exit 2
+	fi
 done
