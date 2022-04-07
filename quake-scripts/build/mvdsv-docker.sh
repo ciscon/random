2413472a96bf67cc69dc8b649f1309698bf9baa6
ciscon <ciscon@nicotinelounge.com>
2022-04-07 18:34:19 -0400

    remove fuse device from docker command, unused here

--- quake-scripts/build/mvdsv-docker.sh
+++ quake-scripts/build/mvdsv-docker.sh
@@ -16,7 +16,7 @@ fi
 
 for arch in $archs;do
 	docker pull $arch/$dist:$release
-	docker run --net=host --rm --device /dev/fuse -v "$gitdir":/mvdsv $arch/$dist:$release bash -c 'export ARCH=$(dpkg --print-architecture);export DEBIAN_FRONTEND=noninteractive;mkdir -p /etc/apt/apt.conf.d;echo "APT::Install-Recommends "0"; APT::AutoRemove::RecommendsImportant "false";" >> /etc/apt/apt.conf.d/01lean && apt-get -qqy update && apt-get -qqy dist-upgrade && apt-get -qqy install cmake build-essential libcurl4-openssl-dev && ln -sf "$(which make)" /usr/bin/gmake && cd /mvdsv && cmake -B build-$ARCH && cd build-$ARCH && make -j$(nproc) && chown -Rf '$(id -u ${USER})':'$(id -g ${USER})' /mvdsv/build*'
+	docker run --net=host --rm -v "$gitdir":/mvdsv $arch/$dist:$release bash -c 'export ARCH=$(dpkg --print-architecture);export DEBIAN_FRONTEND=noninteractive;mkdir -p /etc/apt/apt.conf.d;echo "APT::Install-Recommends "0"; APT::AutoRemove::RecommendsImportant "false";" >> /etc/apt/apt.conf.d/01lean && apt-get -qqy update && apt-get -qqy dist-upgrade && apt-get -qqy install cmake build-essential libcurl4-openssl-dev && ln -sf "$(which make)" /usr/bin/gmake && cd /mvdsv && cmake -B build-$ARCH && cd build-$ARCH && make -j$(nproc) && chown -Rf '$(id -u ${USER})':'$(id -g ${USER})' /mvdsv/build*'
 	errorcode=$?
 	if [ $errorcode -ne 0 ];then
 		echo "$arch $errorcode"
