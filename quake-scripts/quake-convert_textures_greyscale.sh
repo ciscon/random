fdb20fa5032216ce6633648864381f3312dcf029
ciscon <ciscon@nicotinelounge.com>
2022-05-27 12:55:32 -0400

    simplify greyscale conversion script

--- quake-scripts/quake-convert_textures_greyscale.sh
+++ quake-scripts/quake-convert_textures_greyscale.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 
+tmpdir="/tmp/quakeconvert"
+id1="./id1"
+
+#uses the following programs
+uses="bsputil qpakman mogrify zip"
+
+#test for needed programs
+for program in $uses;do
+    if ! hash $program 2>/dev/null;then
+        echo "$program not installed.  bailing out."
+        exit 1
+    fi
+done
+
 olddir=$(pwd)
 
-rm -rf /tmp/quakeconvert
-mkdir -p /tmp/quakeconvert
-cd /tmp/quakeconvert
+rm -rf $tmpdir
+mkdir -p $tmpdir
+cd $tmpdir
 
-cp /download/games/quake/id1/*.pak . || exit
+cp /mnt/nas-quake/id1/*.pak . || exit
 
-for i in *.pak;do pakextract $i;done  
-cd maps 
-for i in *.bsp;do bsputil --extract-textures $i;done  
-for i in *.wad;do qpakman -e $i;done  
-for i in *.png;do convert $i -set colorspace Gray -separate -average $i;done  
+for i in *.pak;do qpakman -f -e $i;done
+cd maps
+for i in *.bsp;do bsputil --extract-textures $i;done
+for i in *.wad;do qpakman -f -e $i;done
+mogrify -set colorspace Gray -separate -average *.png
 mkdir textures  
 mv *.png textures  
-zip -r greyscale_textures.pk3 textures
+zip -r -9 $tmpdir/greyscale_textures.pk3 textures
 
 cd "$olddir"
 
-cp /tmp/quakeconvert/greyscale_textures.pk3 .
+cp $tmpdir/greyscale_textures.pk3 $id1/.
+
+echo "greyscale pk3 is now in $id1"
