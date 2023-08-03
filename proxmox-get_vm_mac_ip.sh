7bc5d93c3c34d0b0df64718a7c533683e43a5a49
ciscon <ciscon@nicotinelounge.com>
2023-08-03 17:38:45 -0400

    use jq to parse qm output for more reliable ip/mac retrieval

--- proxmox-get_vm_mac_ip.sh
+++ proxmox-get_vm_mac_ip.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 # ssh to proxmox hosts and retrieve mac/ip addresses for all vms
 
-#vm network prefix
-network_prefix="10.10.10."
+#network filter to grab the correct address from vm config
+network_filter="10."
 
 #vm hosts
 hosts="
@@ -11,6 +11,14 @@ pve2
 pve3
 pve4
 "
+
+deps="ssh jq tr grep"
+for dep in $deps;do
+  if ! hash $dep >/dev/null 2>&1;then
+    echo "missing dep $dep, bailing out."
+    exit 1
+  fi
+done
 
 vms=
 for i in $hosts;do
@@ -26,21 +34,25 @@ for i in $hosts;do
 				id=$(echo "$line"|awk '{print $1}')
 				name=$(echo "$line"|awk '{print $2}')
 
-				net=$(eval $qm_command guest cmd $id network-get-interfaces 2>/dev/null|grep -B1 -A2 --color=never '"ip-addresses"'|egrep --color=never -e '"ip-address"' -e '"hardware-address"'|grep -B1 --color=never "\"$network_prefix")
-				mac=$(echo "$net"|head -n1|awk -F'"' '{print $4}')
-				ip=$(echo "$net"|tail -n1|awk -F'"' '{print $4}')
+				netraw=$(eval $qm_command guest cmd $id network-get-interfaces) 2>/dev/null
+				net=$(echo "$netraw"|jq -r '.[]|select( ."hardware-address" != null )|{(."hardware-address"):[.["ip-addresses"]|.[]|."ip-address"]}|to_entries[] | [.key] + (.value[]|[.]) | @csv' 2>/dev/null|tr -d '"'|grep --color=never ",$network_filter")
+				if [ ! -z "$net" ];then
+					mac=$(echo "$net"|head -n1|awk -F',' '{print $1}')
+					ip=$(echo "$net"|tail -n1|awk -F',' '{print $2}')
+				fi
+				mac=${mac:-UNKNOWN}
+				ip=${ip:-UNKNOWN}
+
 				hostinfo=$(eval $qm_command guest cmd $id get-osinfo 2>/dev/null)
 				os=$(echo "$hostinfo"|grep --color=never '"id"'|awk -F'"' '{print $4}')
 				version=$(echo "$hostinfo"|grep --color=never '"version-id"'|awk -F'"' '{print $4}')
 				if [ ! -z "$version" ];then
 					version="-${version}"
 				fi
-				if [ ! -z "$ip" ];then
-					echo -e "$i\t$name\t$ip\t$mac\t$id\t${os}${version}"
-				fi
+			  echo -e "$i\t$name\t$ip\t$mac\t$id\t${os}${version}"
 			done
 		)&
 	fi
-done|sort
+done
 
 wait
