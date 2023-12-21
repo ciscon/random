5b4bdcf999cd71fb37947348e7dcbfc3aed4cc24
ciscon <ciscon@nicotinelounge.com>
2023-12-21 11:51:27 -0500

    add deps

--- quake-scripts/quake-badplace_find_users_server.sh
+++ quake-scripts/quake-badplace_find_users_server.sh
@@ -3,6 +3,14 @@
 searchUser="player"
 waitTime=60
 
+deps="wget jq notify-send"
+for dep in $deps;do
+    if ! hash $dep >/dev/null 2>&1;then
+        echo "missing dep $dep, bailing out."
+        exit 1
+    fi
+done
+
 while [ 1 ];do
   found=$(wget -q -O - 'https://badplace.eu/api/v2/serverbrowser/busy'| \
       jq '.[] as $parent | $parent.Players[].Name | select(. == "'$searchUser'") | $parent.Address')
