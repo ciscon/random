32acc186a31e335c409650248782308c8607ab64
ciscon <ciscon@nicotinelounge.com>
2024-01-08 11:22:00 -0500

    minor updates

--- quake-scripts/docker/xorg_with_mounted_mesa_from_host/init.sh
+++ quake-scripts/docker/xorg_with_mounted_mesa_from_host/init.sh
@@ -1,5 +1,5 @@
 cd /build/mesa && \
 	git clean -qfdx && \
-	meson setup --reconfigure builddir/ -Dgallium-drivers=radeonsi -Dvulkan-drivers= -Dplatforms=x11 && \
+	meson setup builddir/ -Dgallium-drivers=radeonsi -Dvulkan-drivers= -Dplatforms=x11 -Degl=false && \
 	meson compile -C builddir/ && \
-	meson devenv -C builddir xinit /home/quakeuser/quake/ezQuake-x86_64.AppImage -- :666
+	meson devenv -C builddir xinit /home/quakeuser/quake/ezQuake-x86_64.AppImage +playdemo fps3 -- :666
