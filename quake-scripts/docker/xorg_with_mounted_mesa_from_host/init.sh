68656107f02d9df31b1372914690b4400944a352
ciscon <ciscon@nicotinelounge.com>
2024-01-12 13:25:40 -0500

    support compiling mesa's libdrm

--- quake-scripts/docker/xorg_with_mounted_mesa_from_host/init.sh
+++ quake-scripts/docker/xorg_with_mounted_mesa_from_host/init.sh
@@ -1,5 +1,6 @@
 cd /build/mesa && \
 	git clean -qfdx && \
+	bash ./.gitlab-ci/container/build-libdrm.sh ; \
 	meson setup builddir/ -Dgallium-drivers=radeonsi -Dvulkan-drivers= -Dplatforms=x11 -Degl=false && \
 	meson compile -C builddir/ && \
 	meson devenv -C builddir xinit /home/quakeuser/quake/ezQuake-x86_64.AppImage +playdemo fps3 -- :666
