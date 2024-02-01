eead70e324d29ad07c7721680d373d95e39549f5
ciscon <ciscon@nicotinelounge.com>
2024-02-01 13:40:34 -0500

    update

--- curl-convert-webkitform.sh
+++ curl-convert-webkitform.sh
@@ -1,2 +1,1 @@
-cat asdf.txt|sed -ne 's/WebKitFormBoundary/\n/gp' |sed -ne 's/.*; name="\([^"]\+\)".*\\r\\n\\r\\n\([^\\]*\)\\r\\n.*/-F '"'"'\1=\2'"'"'/gp'|tr '\n' ' '
-
+cat asdf.txt|sed -ne 's/WebKitFormBoundary/\n/gp' |sed -ne 's/.*form-data; name="\([^"]\+\)".*\\r\\n\\r\\n\(.*\)\\r\\n.*$/-F '"'"'\1=\2'"'"'/gp'|tr '\n' ' '
