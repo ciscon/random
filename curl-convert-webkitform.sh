b9a67be5d8c70bb9ff9d05a9acdf3c58487f1279
ciscon <ciscon@nicotinelounge.com>
2024-02-01 14:04:36 -0500

    escape strings properly

--- curl-convert-webkitform.sh
+++ curl-convert-webkitform.sh
@@ -1,1 +1,12 @@
-cat asdf.txt|sed -ne 's/WebKitFormBoundary/\n/gp' |sed -ne 's/.*form-data; name="\([^"]\+\)".*\\r\\n\\r\\n\(.*\)\\r\\n.*$/-F '"'"'\1=\2'"'"'/gp'|tr '\n' ' '
+#!/bin/bash
+
+filename="asdf.txt"
+
+command=$(cat "$filename")
+
+shortcommand=$(echo "$command"|grep -v --color=never 'WebKitFormBoundary')
+
+fields=$(echo "$command"|sed -ne 's/WebKitFormBoundary/\n/gp' |sed -ne 's/.*form-data; name="\([^"]\+\)".*\\r\\n\\r\\n\(.*\)\\r\\n.*$/-F '"'"'\1=\2'"'"'/gp'|sed 's/\\"/\"/g'|tr '\n' ' ')
+
+echo "$shortcommand $fields"
+
