3779a2aa167a258a22be9ea10816ec6e8f0f1439
ciscon <ciscon@nicotinelounge.com>
2024-03-04 16:14:35 -0500

    support firefox

--- curl-convert-webkitform.sh
+++ curl-convert-webkitform.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-filename="asdf.txt"
+filename="test.txt"
 
 command=$(cat "$filename")
 
-shortcommand=$(echo "$command"|grep -v --color=never 'WebKitFormBoundary')
+if [ $(grep -c 'WebKitFormBoundary' "$filename") -gt 0 ];then
+  shortcommand=$(echo "$command"|grep -v --color=never 'WebKitFormBoundary')
+  fields=$(echo "$command"|sed -ne 's/WebKitFormBoundary/\n/gp' |sed -ne 's/.*form-data; name="\([^"]\+\)".*\\r\\n\\r\\n\(.*\)\\r\\n.*$/-F '"'"'\1=\2'"'"'/gp'|sed 's/\\"/\"/g'|tr '\n' ' ')
+else
+  shortcommand=$(echo "$command"|sed -e 's/--data-binary.*/\n/g')
+  command=$(echo "$command"|sed -e 's/.*--data-binary/\n/g'|sed -e 's/\([-]\+\)[0-9]\+\\r\\n/\1WebKitFormBoundary\\r\\n/g'|sed -e 's/\\r\\n[-]\+[0-9]\+[-]\+//g')
+  fields=$(echo "$command"|sed -ne 's/WebKitFormBoundary/\n/gp' |sed -ne 's/.*form-data; name="\([^"]\+\)"[^\r\n]*\\r\\n\(.*\)\\r\\n.*/-F '"'"'\1=\2'"'"'/gp'|sed 's/\\"/\"/g'|tr '\n' ' ')
+fi
 
-fields=$(echo "$command"|sed -ne 's/WebKitFormBoundary/\n/gp' |sed -ne 's/.*form-data; name="\([^"]\+\)".*\\r\\n\\r\\n\(.*\)\\r\\n.*$/-F '"'"'\1=\2'"'"'/gp'|sed 's/\\"/\"/g'|tr '\n' ' ')
-
-echo "$shortcommand $fields"
+echo "$shortcommand $fields" | sed 's/\\$//g' |tr '\n' ' '
 
