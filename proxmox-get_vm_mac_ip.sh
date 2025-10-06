a99dbf1eed028f4c8fe31fb22a463ccae45396e4
ciscon <ciscon@nicotinelounge.com>
2025-10-06 12:06:38 -0400

    add tags and description to proxmox script

--- proxmox-get_vm_mac_ip.sh
+++ proxmox-get_vm_mac_ip.sh
@@ -36,6 +36,7 @@ for i in $hosts;do
 				id=$(echo "$line"|awk '{print $1}')
 				name=$(echo "$line"|awk '{print $2}')
 				netraw=$(eval $qm_command guest cmd $id network-get-interfaces) 2>/dev/null
+				config=$(eval $qm_command config $id) 2>/dev/null
 				if [ ! -z "$netraw" ];then
 					net=$(echo "$netraw"|jq -r '.[]|select( ."hardware-address" != null )|select ( .["ip-addresses"] != null)|{(."hardware-address"):[.["ip-addresses"]|.[]|."ip-address"]}|to_entries[] | [.key] + (.value[]|[.]) | @csv' 2>/dev/null|tr -d '"'|grep --color=never ",$network_filter")
 					if [ ! -z "$net" ];then
@@ -52,10 +53,12 @@ for i in $hosts;do
 				hostinfo=$(eval $qm_command guest cmd $id get-osinfo 2>/dev/null)
 				os=$(echo "$hostinfo"|grep --color=never '"id"'|awk -F'"' '{print $4}')
 				version=$(echo "$hostinfo"|grep --color=never '"version-id"'|awk -F'"' '{print $4}')
+				tags=$(echo "$config"|grep --color=never "^tags:"|awk '{print $2}')
+				description=$(echo "$config"|grep --color=never "^description:"|awk '{print $2}')
 				if [ ! -z "$version" ];then
 					version="-${version}"
 				fi
-			  echo -e "$i\t$name\t$ip\t$mac\t$id\t${os}${version}"
+			  echo -e "$i\t$name\t$ip\t$mac\t$id\t${os}${version}\t${tags}\t${description}"
 			done
 		)&
 	fi
