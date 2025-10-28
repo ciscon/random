cb6f311374a6d03deda605fb3fc7f67734c569f9
ciscon <ciscon@nicotinelounge.com>
2025-10-28 14:23:50 -0400

    symlinks

--- browser/chrome.sh
+++ browser/chrome.sh
@@ -4,4 +4,5 @@ set -e
 read -p "version (eg 128): " version
 
 npx @puppeteer/browsers --path=/tmp/browsers install chrome@${version}
+ln -sf "$(ls /tmp/browsers/chrome/linux-${version}*/chrome-linux64/chrome|head -n1)" "$HOME/Desktop/chrome-${version}"
 /tmp/browsers/chrome/*linux-${version}*/chrome-linux64/chrome
--- browser/firefox.sh
+++ browser/firefox.sh
@@ -4,4 +4,5 @@ set -e
 read -p "version (eg 128.0): " version
 
 npx @puppeteer/browsers --path=/tmp/browsers install firefox@stable_${version}
+ln -sf "$(ls /tmp/browsers/firefox/*stable_${version}*/firefox/firefox|head -n1)" "$HOME/Desktop/chrome-${version}"
 /tmp/browsers/firefox/*stable_${version}*/firefox/firefox
