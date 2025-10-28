123fbd2f11bfd881cc95f32fdfa8e7648a3081af
ciscon <ciscon@nicotinelounge.com>
2025-10-28 14:44:25 -0400

    ignore platform

--- browser/chrome.sh
+++ browser/chrome.sh
@@ -3,9 +3,13 @@ set -e
 
 browser_path="/var/tmp/browsers"
 
-read -p "version (eg 128): " version
+if [ -z $1 ];then
+	read -p "version (eg 128): " version
+else
+	version=$1
+fi
 
 npx @puppeteer/browsers --path="$browser_path" install chrome@${version}
-browser=$(ls "$browser_path/chrome/linux-${version}"*"/chrome-linux64/chrome"|head -n1)
+browser=$(ls "$browser_path/chrome/"*"-${version}"*"/chrome-"*"/chrome"|head -n1)
 ln -sf "$browser" "$HOME/Desktop/chrome-${version}"
 "$browser"
--- browser/firefox.sh
+++ browser/firefox.sh
@@ -3,9 +3,13 @@ set -e
 
 browser_path="/var/tmp/browsers"
 
-read -p "version (eg 128.0): " version
+if [ -z $1 ];then
+  read -p "version (eg 128.0): " version
+else
+  version=$1
+fi
 
 npx @puppeteer/browsers --path="$browser_path" install firefox@stable_${version}
-browser=$(ls "$browser_path/firefox/linux-stable_${version}"*"/firefox/firefox"|head -n1)
+browser=$(ls "$browser_path/firefox/"*"-stable_${version}"*"/firefox/firefox"|head -n1)
 ln -sf "$browser" "$HOME/Desktop/firefox-${version}"
 "$browser"
