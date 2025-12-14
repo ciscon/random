80b76d9179fede97cc9ff9463075e21e5bdae091
ciscon <ciscon@nicotinelounge.com>
2025-12-14 18:28:03 -0500

    browser script - add osx

---
+++ browser/firefox-osx.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+browser_path="/var/tmp/browsers"
+if [ -z $1 ];then
+  read -p "version (eg 128.0): " version
+else
+  version=$1
+fi
+npx @puppeteer/browsers --path="$browser_path" install firefox@stable_${version}
+browser=$(ls "$browser_path/firefox/"*"-stable_${version}"*"/Firefox.app/Contents/MacOS/firefox"|tail -n1)
+browser_installed_dir=$(dirname "${browser}")
+echo disabled > "${browser_installed_dir}/update-settings.ini"
+ln -sf "$browser" "$HOME/Desktop/firefox-${version}"
+"$browser"
